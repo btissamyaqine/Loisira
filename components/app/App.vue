8548d00c0377edeac5048f8e19d9f52037425bb1
Btissam Yaqine <btissamyaqine123@gmail.com>
2022-10-19 12:36:54 +0100

    filter done

--- components/app/App.vue
+++ components/app/App.vue
@@ -15,53 +15,46 @@
         <button type="submit" class="top-search-btn">Rechercher</button>
       </div>
     </div>
-hey am from filter
-
-    <Filters :filterCards="filterCards"  />
-    <div>
-      <p v-for="filter in filters" :key="filter" @click="() => filterCards(filter)">
-        {{filter}}
-      </p>
-    </div>
-    <!-- <div class="activites">
+   
+    <div class="activites">
           <div class="title-activite">
             <h1>Recherche par activite</h1>
           </div>
           <div class="activite-wrapper">
-            <div class="activite" @click="toggel($event)">
+            <div class="activite" @click="cardsFilter($event)" >
               <i class="fas fa-skiing" >
                 <div class="items" >Sport hiver</div>
               </i>
             </div>
-            <div class="activite" @click="toggel($event)">
+            <div class="activite" @click="cardsFilter($event)">
               <i class="fas fa-swimmer">
                 <div class="items">Aquatique</div>
               </i>
             </div>
-            <div class="activite" @click="toggel($event)">
+            <div class="activite" @click="cardsFilter($event)">
               <i class="fas fa-skiing-nordic">
                 <div class="items">Randonnée</div>
               </i>
             </div>
-            <div class="activite" @click="toggel($event)">
+            <div class="activite" @click="cardsFilter($event)">
               <i class="fas fa-parachute-box">
                 <div class="items">Aérien</div>
               </i>
             </div>
-            <div class="activite" @click="toggel($event)">
+            <div class="activite" @click="cardsFilter($event)">
               <i class="fas fa-basketball-ball">
                 <div class="items">Sport d'équipe</div>
               </i>
             </div>
           </div>
-      </div> -->
-      <div class="cards">
+      </div>
+      <div v-if="this.cardsFilterKey?.length > 0" :key="compKey" class="cards">
       <div class="title-card">
         <h1>Nos coups de coeur</h1>
       </div>
       <div class="card-container">
         <ul class="card-wrapper">
-          <li v-for="card in searchCards" :key="card.id" :card="card" class="card">
+          <li v-for="card in (filtred)" :key="card.id" :card="filtred" class="card">
             <img :src="card.photo.url" alt=''>
             <div class="card-content">
               <div class="content-top">
@@ -86,13 +79,13 @@ hey am from filter
                   <h3>1h</h3>
                 </div>
               </div>
-                
             </div>
           </li>
         </ul>
       </div>
       
     </div>
+    <div v-else >There are no post left. Have you tried adding a category?</div>
     
     <Client />
     <Footer />
@@ -107,7 +100,6 @@ hey am from filter
   import Footer from './Footer.vue'
   import HeaderList from "../elements/HeaderList.vue"
   import Filters from './filters.vue'
-  // const filters = []
 
    
   export default {
@@ -116,29 +108,43 @@ hey am from filter
     data: () => ({
       cards:[],
       search:'',
-      filters:['All', 'Sport', 'Aquatique']
+      cardsFilterKey: ['Aquatique','Aérien', 'Randonnée', 'Sport hiver'],
+      compKey: 0
     }),
     computed: {
-        searchCards() {
-            return this.cards.filter(card => card.ville.toLowerCase().includes(this.search.toLowerCase()) || card.pay.toLowerCase().includes(this.search.toLowerCase()))
+        filtred() {
+          if(this.search != '' && this.search) {
+            return this.cards.filter(card => card.ville.toLowerCase().includes(this.search.toLowerCase()) || card.pay.toLowerCase().includes(this.search.toLowerCase()));
+
+          };
+          if(this.cardsFilterKey?.length == 0){
+            return;
+          } else {
+            return this.cards.filter(card => this.cardsFilterKey.includes(card.categorie))
           }
+        }
+          
         },
     async fetch() {
     this.cards = await this.$axios.$get('http://localhost:4000/cards/')
+
     }, 
     methods: {
-      filterCards(catName) {
-      this.resetCards()
-      if (catName !== 'All') {
-        this.cards = this.cards?.filter((card) => {
-          return card.categorie === catName
-        })
+      cardsFilter(event) {
+        this.compKey++;
+        const element = event.target;
+
+        const text = element.textContent.trim()
+        console.log('yay you on this ' + text);
+        if(this.cardsFilterKey.includes(text)) {
+          this.cardsFilterKey.splice(this.cardsFilterKey.indexOf(text), 1)
+        } else {
+          this.cardsFilterKey.push(text)
+          // console.log(this.cardsFilterKey)
+        }
       }
-    },
-    resetCards() {
-      this.cards = this.$axios.$get('http://localhost:4000/cards/') 
     }
-  }
+
   }  
 </script>
 <style>
